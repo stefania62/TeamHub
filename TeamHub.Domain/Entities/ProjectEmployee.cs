42b788d1317f39be824db59ca00198376d8b71b9
Stefani Ropi <stefani.ropi@pragmatic.al>
2025-03-21 09:51:57 +0100

    Move entities to TeamHub.Domain

---
+++ TeamHub.Domain/Entities/ProjectEmployee.cs
@@ -0,0 +1,13 @@
+﻿namespace TeamHub.API.Entities
+{
+    /// <summary>
+    /// Represents the relationship between Employees and Projects.
+    /// </summary>
+    public class ProjectEmployee
+    {
+        public int ProjectId { get; set; }
+        public Project Project { get; set; } = null!;
+        public string EmployeeId { get; set; } = string.Empty;
+        public ApplicationUser Employee { get; set; } = null!;
+    }
+}
